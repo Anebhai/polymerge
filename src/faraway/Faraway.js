cd4f5cde6b9b2e30967a238e45e3ef777d3d4e97
Anebhai <ane.iqbal@gmail.com>
2023-09-06 19:11:32 +0530

    Faraway components done for placeholding

--- src/faraway/Faraway.js
+++ src/faraway/Faraway.js
@@ -2,9 +2,38 @@ import "./faraway.css";
 
 function Faraway() {
   return (
-    <div>
-      <h1>Faraway</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <h1> 🌴 Far Away 💼</h1>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you want for your Trip</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      💼you have x items on your list, and you already packed (X%) items
+    </footer>
   );
 }
 
